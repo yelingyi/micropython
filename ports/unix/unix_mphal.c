694b941afba0a5008328b0228436612cbcd00f2f
Andrew Leech <andrew@alelec.net>
2021-09-14 13:32:02 +1000

    windows/thread: Add support for win32 micropython _thread.

--- ports/unix/unix_mphal.c
+++ ports/unix/unix_mphal.c
@@ -225,12 +225,12 @@ void mp_hal_delay_ms(mp_uint_t ms) {
     #ifdef MICROPY_EVENT_POLL_HOOK
     mp_uint_t start = mp_hal_ticks_ms();
     while (mp_hal_ticks_ms() - start < ms) {
-        // MICROPY_EVENT_POLL_HOOK does mp_hal_delay_us(500) (i.e. usleep(500)).
+        // MICROPY_EVENT_POLL_HOOK does mp_hal_delay_us(500).
         MICROPY_EVENT_POLL_HOOK
     }
     #else
     // TODO: POSIX et al. define usleep() as guaranteedly capable only of 1s sleep:
     // "The useconds argument shall be less than one million."
-    usleep(ms * 1000);
+    mp_hal_delay_us(ms * 1000);
     #endif
 }
