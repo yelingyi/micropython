59eb4baacf36fd4fd2b407909357428d3bf2888b
Matthias Urlichs <matthias@urlichs.de>
2024-08-28 12:48:24 +0200

    asyncio: Move `run_server` to examples.
    `run_server` is not in CPython. We'd like to avoid that.
    
    Signed-off-by: Matthias Urlichs <matthias@urlichs.de>

--- extmod/asyncio/stream.py
+++ extmod/asyncio/stream.py
@@ -212,47 +212,6 @@ async def start_server(cb, host, port, backlog=5, ssl=None):
     return srv
 
 
-# Helper task to run a TCP stream server.
-# Callbacks (i.e. connection handlers) may run in a different taskgroup.
-async def run_server(cb, host, port, backlog=5, taskgroup=None):
-    import socket
-
-    # Create and bind server socket.
-    host = socket.getaddrinfo(host, port)[0]  # TODO this is blocking!
-    s = socket.socket()
-    s.setblocking(False)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(host[-1])
-    s.listen(backlog)
-
-    if taskgroup is None:
-        from . import TaskGroup
-
-        async with TaskGroup() as tg:
-            await _run_server(tg, s, cb)
-    else:
-        await _run_server(taskgroup, s, cb)
-
-
-async def _run_server(tg, s, cb):
-    while True:
-        try:
-            yield core._io_queue.queue_read(s)
-        except core.CancelledError:
-            # Shutdown server
-            s.close()
-            return
-        try:
-            s2, addr = s.accept()
-        except Exception:
-            # Ignore a failed accept
-            continue
-
-        s2.setblocking(False)
-        s2s = Stream(s2, {"peername": addr})
-        tg.create_task(cb(s2s, s2s))
-
-
 ################################################################################
 # Legacy uasyncio compatibility
 
