806cf73ea6ebe188eb69e770c22510f9f0d19200
Keenan Johnson <keenan.johnson@gmail.com>
2024-11-17 16:44:19 -0800

    ssl_dtls: Improve test

--- tests/ports/esp32/ssl_dtls.py
+++ tests/ports/esp32/ssl_dtls.py
@@ -6,6 +6,11 @@ except ImportError:
     print("SKIP")
     raise SystemExit
 
+# Check that the ssl module has the DTLS constants.
+if not hasattr(ssl, 'PROTOCOL_DTLS_CLIENT') or not hasattr(ssl, 'PROTOCOL_DTLS_SERVER'):
+    print("SKIP")
+    raise SystemExit
+
 # Test constructing with arguments.
 dtls_client = ssl.SSLContext(ssl.PROTOCOL_DTLS_CLIENT)
 dtls_server = ssl.SSLContext(ssl.PROTOCOL_DTLS_SERVER)
