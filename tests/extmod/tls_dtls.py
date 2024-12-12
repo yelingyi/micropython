b20d5de51538c45d37a48583c5960d58da365c14
Keenan Johnson <keenan.johnson@gmail.com>
2024-12-12 02:29:08 +0000

    Improve test by wrapping the client and server

--- tests/extmod/tls_dtls.py
+++ tests/extmod/tls_dtls.py
@@ -1,14 +1,22 @@
 # Very basic test of DTLS.
 
 try:
-    from tls import PROTOCOL_DTLS_CLIENT, PROTOCOL_DTLS_SERVER, SSLContext
+    from tls import PROTOCOL_DTLS_CLIENT, PROTOCOL_DTLS_SERVER, SSLContext, CERT_NONE
+    import socket
 except ImportError:
     print("SKIP")
     raise SystemExit
 
+# Wrap the DTLS Client
+dtls_client_ctx = SSLContext(PROTOCOL_DTLS_CLIENT)
+dtls_client_ctx.verify_mode = CERT_NONE
+dtls_client = dtls_client_ctx.wrap_socket(socket.socket(), do_handshake_on_connect=False)
+print('Wrapped DTLS Client')
 
-# Test constructing with arguments.
-dtls_client = SSLContext(PROTOCOL_DTLS_CLIENT)
-dtls_server = SSLContext(PROTOCOL_DTLS_SERVER)
+# Wrap the DTLS Server
+dtls_server_ctx = SSLContext(PROTOCOL_DTLS_SERVER)
+dtls_server_ctx.verify_mode = CERT_NONE
+dtls_server = dtls_server_ctx.wrap_socket(socket.socket(), do_handshake_on_connect=False)
+print('Wrapped DTLS Server')
 
 print('OK')
