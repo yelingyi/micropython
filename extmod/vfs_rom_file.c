b5b1ca0ef91b70efee3d03e2b98a74eb4e155411
Damien George <damien@micropython.org>
2024-12-20 16:20:57 +1100

    extmod/vfs_rom_file: Don't cast to ssize_t, it's already signed.
    Signed-off-by: Damien George <damien@micropython.org>

--- extmod/vfs_rom_file.c
+++ extmod/vfs_rom_file.c
@@ -118,7 +118,7 @@ static mp_uint_t vfs_rom_file_ioctl(mp_obj_t o_in, mp_uint_t request, uintptr_t 
                 self->file_offset = self->file_size + s->offset;
             }
             if (self->file_offset > self->file_size) {
-                if ((ssize_t)s->offset < 0) {
+                if (s->offset < 0) {
                     // Seek to before the start of the file.
                     *errcode = MP_EINVAL;
                     return MP_STREAM_ERROR;
